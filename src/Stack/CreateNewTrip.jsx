959cedb92b8a56e4803400563c0b1b5dd61eb90c
Naveesha Lakshan <lakshannaveesha10@gmail.com>
2025-05-10 21:48:45 +0530

    Start create new trip form

--- src/Stack/CreateNewTrip.jsx
+++ src/Stack/CreateNewTrip.jsx
@@ -1,10 +1,18 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput } from "react-native";
 import React from "react";
 
 const CreateNewTrip = () => {
   return (
     <View>
-      <Text>CreateNewTrip</Text>
+      <TextInput
+        placeholder="Trip Name"
+        style={{
+          borderWidth: 1,
+          borderColor: "#000",
+          padding: 10,
+          marginBottom: 20,
+        }}
+      />
     </View>
   );
 };
